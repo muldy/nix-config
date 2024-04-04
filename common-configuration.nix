347f8c6d7705f8132aa2d82a7297668a9999dc86
muldy <muldy@thinkpad-x260>
2024-04-04 23:51:49 +0100

    more changes

--- common-configuration.nix
+++ common-configuration.nix
@@ -2,6 +2,17 @@
 
 { config, pkgs, ... }:
 {
+  boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+   pname = "distro-grub-themes";
+   version = "3.1";
+   src = pkgs.fetchFromGitHub {
+     owner = "AdisonCavani";
+     repo = "distro-grub-themes";
+     rev = "v3.1";
+     hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+   };
+   installPhase = "cp -r customize/nixos $out";
+  };
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -79,6 +90,8 @@
     python3
     python311Packages.virtualenv
     dig
+    firefox
+    thunderbird
   ];
   programs.tmux = {
     enable = true;
