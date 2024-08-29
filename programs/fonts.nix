247cfdaadae69eb15af1e14f2d75fdda534b11bc
muldy <muldy@t14>
2024-08-29 11:48:28 +0100

    alternative

--- programs/fonts.nix
+++ programs/fonts.nix
@@ -5,23 +5,48 @@
     fontconfig = {
       enable = true;
       localConf = ''
-      <?xml version="1.0"?>
-      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-      <fontconfig>
-        <match target="font">
-          <edit name="autohint" mode="assign">
-            <bool>true</bool>
-          </edit>
-        </match>
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+        <fontconfig>
+
+          <!-- Not sure what this does -->
+          <match target="scan">
+            <test name="family">
+              <string>Noto Color Emoji</string>
+            </test>
+            <edit name="scalable" mode="assign">
+              <bool>true</bool>
+            </edit>
+          </match>
+
+          <!-- Add icons as first font for any missing glyphs -->
+          <match target="pattern">
+            <edit name="family" mode="prepend_first">
+              <string>Icons</string>
+            </edit>
+          </match>
+            
+          <!-- Want great emojies! -->
+          <match target="pattern">
+            <edit name="family" mode="prepend_first">
+              <string>Noto Color Emoji</string>
+            </edit>
+          </match>
+
         <alias>
-          <family>MyFont</family>
-          <prefer>
-            <family>Ubuntu</family>
-            <family>Symbols Nerd Font</family>
-            <family>Noto Color Emoji</family>
-          </prefer>
+          <family>monospace</family>
+            <prefer>
+              <family>Ubuntu</family>
+            </prefer>
         </alias>
-      </fontconfig>
+
+        <alias>
+          <family>serif</family>
+            <prefer>
+              <family>Ubuntu</family>
+            </prefer>
+        </alias>
+        </fontconfig>
       '';
       defaultFonts = {
         emoji = [ "Noto Color Emoji" ];
