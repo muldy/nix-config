f6f5917dee3067e7c5b1aa8dce652ca8cc68484e
Muldy <muldy@urmeta.eu>
2024-09-22 22:01:18 +0100

    step

--- hosts/MacBook-Pro/home.nix
+++ hosts/MacBook-Pro/home.nix
@@ -5,5 +5,24 @@
   imports = 
   [
       ../../pkg-config/git.nix
-  ]
+      ../../pkg-config/neovim.nix
+      ../../pkg-config/kitty.nix
+      ../../pkg-config/eza.nix
+      ../../pkg-config/tmux.nix
+      ../../pkg-config/oh-my-posh.nix
+      ../../pkg-config/zsh.nix
+  ];
+  home.file = {
+    ".config/ohmyposh/zen.toml".source = ../../dotfiles/oh-my-posh.toml;
+    ".config/variety/variety.conf".source = ../../dotfiles/variety.conf;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.bash.enable = true;
 }
