d7e63d791c381c554c76bba7af92f465b34f9eb8
Miguel Fernandes <extern.fernandes_miguel@allianz.de>
2024-11-26 08:54:00 +0000

    kittya tabs mac

--- hosts/MacBook-Pro/home.nix
+++ hosts/MacBook-Pro/home.nix
@@ -52,6 +52,7 @@
   home.file = {
     ".config/ohmyposh/zen.toml".source = ../../dotfiles/oh-my-posh.toml;
     ".config/variety/variety.conf".source = ../../dotfiles/variety.conf;
+    ".config/kitty/tab_bar.py".source = ../../dotfiles/tab_bar.py;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
