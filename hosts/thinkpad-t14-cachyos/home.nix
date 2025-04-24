e5ee7728d93add83dbe30359f555131c0e9714b3
muldy <mrodferna@gmail.com>
2025-04-24 13:01:40 +0100

    enable variety

--- hosts/thinkpad-t14-cachyos/home.nix
+++ hosts/thinkpad-t14-cachyos/home.nix
@@ -101,7 +101,7 @@
 
   home.file = {
     #".config/ohmyposh/zen.toml".source = ../../dotfiles/oh-my-posh.toml;
-    #".config/variety/variety.conf".source = ../../dotfiles/variety.conf;
+    ".config/variety/variety.conf".source = ../../dotfiles/variety.conf;
     ".config/kitty/tab_bar.py".source = ../../dotfiles/tab_bar.py;
     ".gitconfig".source = ../../dotfiles/gitconfig;
     ".p10k.zsh".source = ../../dotfiles/p10k.zsh;
